03f3b315d239ca68f92a8ddadf3d57a4a52c4268
ngladkov <ngladkov@tm-serv.ru>
2022-09-22 16:29:16 +0300

    initial commit

---
+++ front/app/components/screens/Home.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react'
+
+import { IHome } from '@/screens/home.interface'
+
+const Home: FC<IHome> = () => {
+	return <div></div>
+}
+
+export default Home
---
+++ front/next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	swcMinify: true,
+	poweredByHeader: false,
+}
+
+module.exports = nextConfig
